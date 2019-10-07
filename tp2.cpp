186cee08168a876e56d30d5bccc889f696605c79
arthursl12 <arthursoutolima@gmail.com>
2019-10-07 09:45:16 -0300

    Implementação do alg. prog. dinâmica - Implemenação do algoritmo de programaçã dinâmica para obtenção dos roteiros - Programa faz o que a especificação manda

--- tp2.cpp
+++ tp2.cpp
@@ -18,15 +18,15 @@
 Problema da Mochila:
     Capacidade: custo máximo
     Objetos: ilhas com seu valor
-- Ordena ilhas em ordem crescente de custo
-- Monta matriz: [5,MAX/100]
+- Monta matriz: [5+1,MAX+1]
 - Preenche linha a linha, coluna a coluna, a partir do [0,0]
 - A resposta é a última célula
 */
 
-/* Particiona a string 'str' pelo delimitador 'delimiter' e guarda o token particionado na string 'token' (o token é removido da string original);
-Se não encontrar o token, apenas coloca a string original no 'token';
-Retorna 'True' se foi possível fazer a operação, 'False' se não encontrar o delimitador e não conseguir fazer o particionamento; */
+/* Particiona a string 'str' pelo delimitador 'delimiter' e guarda o token particionado 
+na string 'token' (o token é removido da string original); Se não encontrar o token, 
+apenas coloca a string original no 'token'; Retorna 'True' se foi possível fazer 
+a operação, 'False' se não encontrar o delimitador e não conseguir fazer o particionamento; */
 bool str_tok(std::string& str, std::string delimiter, std::string& token){
     std::size_t idx = str.find(delimiter);
     token = str.substr(0, idx);
@@ -38,6 +38,75 @@ bool str_tok(std::string& str, std::string delimiter, std::string& token){
     }
     str.erase(0, idx+1); /* Remove o token e o delimitador da string original */
     return true;
+}
+
+typedef struct node{
+    int pontuacao;
+    int dias;
+} node;
+
+/* Tempo: (Lin*Col). Espaço: (Lin*Col) */
+/* Cria uma matriz de 'node', de dimensões 'lin' x 'col' */
+void cria_matriz(std::vector<std::vector<node>>& matriz, int lin, int col){
+    for (int i = 0; i < lin; i++){
+        std::vector<node> linha;
+        matriz.push_back(linha);
+        for (int j = 0; j < col; j++){
+            node nulo;
+            nulo.pontuacao = -1;
+            nulo.dias = -1;
+            matriz[i].push_back(nulo);
+        }
+    }
+}
+
+// https://en.wikipedia.org/wiki/Knapsack_problem
+/* Melhor solução para as 'k' primeiras ilhas e o peso 'peso' */
+void opt(std::vector<std::vector<node>>& matriz, std::vector<ilha>& Ilhas, int k, int peso){
+    int i = k-1; // O índice da ilha só vai até 4
+    /* 0 ilhas ou custo máximo 0 */
+    if ((k == 0) || (peso <= 0)){
+        matriz[k][peso].pontuacao = 0;
+        matriz[k][peso].dias = 0;
+        return;
+    }
+    
+    /* Não calculamos ainda com 'k-1' ilhas e o peso 'peso' */
+    if (matriz[k-1][peso].pontuacao == -1)
+        opt(matriz, Ilhas, k-1, peso);
+
+    /* O custo da nova ilha 'k' é maior que o custo total (peso da mochila) */
+    if (Ilhas[i].custo > peso){
+        matriz[k][peso] = matriz[k-1][peso];
+    }else{
+        /* Não calculamos ainda com 'k-1' ilhas e o peso ('peso'-'peso de k') */
+        if (matriz[k-1][peso-Ilhas[i].custo].pontuacao == -1){
+            opt(matriz, Ilhas, k-1, peso-Ilhas[i].custo);
+        }
+
+        /* Olha o maior entre não colocar a ilha 'k' ou colocá-la */
+        if (matriz[k-1][peso].pontuacao > (matriz[k-1][peso-Ilhas[i].custo].pontuacao + Ilhas[i].valor)){
+            matriz[k][peso] = matriz[k-1][peso];
+        }else{
+            matriz[k][peso].pontuacao = matriz[k-1][peso-Ilhas[i].custo].pontuacao + Ilhas[i].valor;
+            matriz[k][peso].dias = matriz[k-1][peso-Ilhas[i].custo].dias + 1;
+        }
+    }
+}
+
+/* Obtém a melhor pontuação e duração de viagem, dado um conjunto de N ilhas com
+custos e valores associados, além de um custo máximo de gastos; Adota programação
+dinâmica, resolvendo um 'problema da mochila'; */
+void dinamica(int& pontu, int& dias, int Max, int N, std::vector<ilha>& Ilhas){
+    int n_colunas = Max+1; // A 'capacidade' da mochila é o custo máximo total
+    int n_linhas = N+1; // Os 'objetos' são as ilhas com suas pontuações associadas
+
+    std::vector<std::vector<node>> matriz;
+    cria_matriz(matriz, n_linhas, n_colunas);
+
+    opt(matriz, Ilhas, N, Max);
+    pontu = matriz[N][Max].pontuacao;
+    dias = matriz[N][Max].dias;
 }
 
 int main(int argc, char* argv[]){
@@ -76,23 +145,17 @@ int main(int argc, char* argv[]){
 
         Ilhas.push_back(i1);
     }
-    
-    /*
-    for (int i = 0; i < N; i++){
-        std::cout << i << " - " << Ilhas[i].custo << "  " << Ilhas[i].valor << "  " << Ilhas[i].cdv << std::endl;
-    }
-
-    std::cout << "Custo Total: " << C_Max << std::endl;
-    std::cout << "#ilhas: " << N << std::endl << std::endl;
-    */
 
     /* Execução */
     int dias = 0;
     int pontu = 0;
     guloso(pontu, dias, C_Max, N, Ilhas);
     std::cout << pontu << " " << dias << std::endl;
+    dias = 0;
+    pontu = 0;
+    dinamica(pontu, dias, C_Max, N, Ilhas);
+    std::cout << pontu << " " << dias << std::endl;
 
-    
     arq.close();
     return 0;
 }
